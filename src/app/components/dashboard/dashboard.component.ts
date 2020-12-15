04b0b582df9b0b7cafc2b99269f2c6636c75d2f5
Nabeel Ahmad Khan <nabeel.ahmed@odinesolutions.com>
2020-12-16 02:33:24 +0500

    Dashboard UI Added

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as chartData from '../../shared/data/dashboard/ecommerce'
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  sale: any = 5000;
+  profitData: any = 2400;
+  public todayTotalSale = chartData.todayTotalSale;
+  public totalVisit = chartData.totalVisit;
+  public profit = chartData.profit;
+  public riskFactor = chartData.riskFactor;
+
+  public lat_m1: number = 20.593683;
+  public lng_m1: number = 78.962883;
+  public zoom_m1: number = 4;
   constructor() { }
 
   ngOnInit() {
--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -1,14 +1,27 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { ChartistModule } from 'ng-chartist';
+import { ChartsModule } from 'ng2-charts';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
-
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    ChartistModule,
+    ChartsModule,
+    NgApexchartsModule,
     SharedModule,
+    CarouselModule,
+    CKEditorModule,
+    NgbModule,
+    FormsModule,
     DashboardRoutingModule
   ],
   declarations: [DashboardComponent]
